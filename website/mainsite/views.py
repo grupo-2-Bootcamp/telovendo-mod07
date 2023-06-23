9fa4fc800d58a6049a8d864fcc67d680b8398327
Eduardo Escárez <eduardoe@gmail.com>
2023-06-23 02:27:21 -0400

    Punto inicial de mainsite

--- website/mainsite/views.py
+++ website/mainsite/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class Index(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        title = "Bienvenido a TeLoVendo"
+        return render(request, self.template_name, {"title": title})
--- website/website/settings.py
+++ website/website/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC-4'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
