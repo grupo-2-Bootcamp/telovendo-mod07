162a34e2be9ee3ec6d990bbc63c0111889ca7efc
BastianPerezG <bastianesteban.perez@gmail.com>
2023-06-25 11:14:59 -0400

    agregando modelo

--- website/telovendo/models.py
+++ website/telovendo/models.py
@@ -1,3 +1,58 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=45, null=False, blank=False)
+    descripcion = models.CharField(max_length=45, null=False, blank=False)
+    stock = models.IntegerField(null=False, blank=False)
+    precio = models.IntegerField(null=False, blank=False)
+    urlfoto = models.CharField(max_length=45, null=False, blank=False)
+
+
+class Estado_Pedido(models.Model):
+    estado = models.CharField(max_length=45, null=False, blank=False)
+
+
+class MetodoPago(models.Model):
+    nombre = models.CharField(max_length=45, null=False, blank=False)
+
+
+class Empresas(models.Model):
+    run = models.CharField(max_length=10, null=False, blank=False)
+    nombre_empresa = models.CharField(max_length=30, null=False, blank=False)
+
+
+class Users(models.Model):
+    idEmpresa = models.ForeignKey(Empresas, on_delete=models.SET_NULL, null=True)
+    username = models.CharField(max_length=30, null=False, blank=False)
+    first_name = models.CharField(max_length=45, null=False, blank=False)
+    last_name = models.CharField(max_length=45, null=False, blank=False)
+    password = models.CharField(max_length=30, null=False, blank=False)
+    group = models.CharField(max_length=45, null=True)
+
+
+class Direcciones(models.Model):
+    idEmpresa = models.ForeignKey(Empresas, on_delete=models.CASCADE, null=False, blank=False)
+    direccion = models.CharField(max_length=45, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    Comuna = models.CharField(max_length=45, null=False, blank=False)
+    descripcion = models.CharField(max_length=45, null=True)
+
+
+class Pedidos(models.Model):
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    idMetodoPago = models.ForeignKey(MetodoPago, on_delete=models.CASCADE, null=False, blank=False)
+    idEstado = models.ForeignKey(Estado_Pedido, on_delete=models.CASCADE, null=False, blank=False)
+    idDireccion = models.IntegerField(null=False, blank=False)
+    idUsuario = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True)
+    idEmpresa = models.ForeignKey(Empresas, on_delete=models.SET_NULL, null=True)
+    total_pedido = models.IntegerField(null=False, blank=False)
+
+
+class Detalles_Pedido(models.Model):
+    idProductos = models.ForeignKey(Productos, on_delete=models.CASCADE, null=False, blank=False)
+    idPedidos = models.ForeignKey(Pedidos, on_delete=models.CASCADE, null=False, blank=False)
+    cantidad = models.IntegerField(null=False, blank=False)
+    precio = models.IntegerField(null=False, blank=False)
