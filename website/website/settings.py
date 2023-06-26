3b25533b0dab75d801618de2b199a3126fdd4baa
Eduardo Escárez <eduardoe@gmail.com>
2023-06-26 13:22:07 -0400

    Actualiza paginas, ajusta rutas

--- website/website/settings.py
+++ website/website/settings.py
@@ -136,3 +136,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Agrega rutas de redirección
+
+# LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
